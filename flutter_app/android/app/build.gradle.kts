5e3f04d6431aedf7471d54cacc05a41d2953bc35
aeemayo <keemeayo@gmail.com>
2026-06-26 21:28:22 +0100

    modified:   flutter_app/android/app/build.gradle.kts 	modified:   flutter_app/android/app/google-services.json

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.propflow.propflow_app"
+    namespace = "com.propflow.Propflow"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.propflow.propflow_app"
+        applicationId = "com.propflow.Propflow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
